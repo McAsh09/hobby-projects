0930430f2f50b6106a25ad66f9741d3c6e00ce82
McAsh09 <mcash0907@gmail.com>
2021-10-07 23:09:21 +0530

    init commit

---
+++ src/components/Project/Project.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Project() {
+    return (
+        <div>
+            Project called
+        </div>
+    )
+}
+
+export default Project
---
+++ src/components/ProjectForm/ProjectForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectForm() {
+    return (
+        <div>
+            Project form called
+        </div>
+    )
+}
+
+export default ProjectForm
