055d8e382c6d4e7c6c5eb2e97755efa278cb4eda
McAsh09 <mcash0907@gmail.com>
2021-10-08 18:03:21 +0530

    updated app file

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import ProjectForm from './components/ProjectForm/ProjectForm';
 import './App.css';
 
 function App() {
-  return (    
+  return (         
       <Switch>
+        {/*Added routes here to access different components */}
         <Route exact path='/' component={Home} />
         <Route path='/project' component={Project} />
-        <Route path='/project-form' component={ProjectForm} />
-      </Switch>   
+        <Route path='/project-form' component={ProjectForm} />         
+      </Switch>     
   );
-}
+  }
 
 export default App;
