0ccacd9460cc5ad90372a3b89b85ef562b7e6739
McAsh09 <mcash0907@gmail.com>
2021-10-08 18:00:10 +0530

    updated index file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // I have renamed BrowserRouter to Router using 'as' here so that it is easy to understand
+import { Provider } from 'react-redux'; //I have imported Provider here so that I can make the state available for the entire app
+import store from './store/index'; //imported since it stores the state of the application
 
 
-ReactDOM.render(
+ReactDOM.render(    
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>
     ,document.getElementById('root'));
 
